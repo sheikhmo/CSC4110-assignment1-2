f95b2523673ec65b37a0baefb9ea65b9b367a547
Mohammad Sheikh <gi8310@wayne.edu>
2021-05-25 22:20:10 -0400

    First commit for assignment 1

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+// Simple C++ program to display "Hello World" 
+//Author: Mohammad Sheikh <gi8310@wayne.edu>
+
+// Header file for input output functions 
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Testing git merge!"; 
+      
+    return 0; 
+} 
